785293aec8ed22d99e0760e17af7cad1d1342efb
Nicolas Guilaume Soulié <nicolas.guillaume.soulie@efrei.net>
2021-02-26 16:47:15 +0100

    Clean up & comments

--- app/src/main/java/com/example/flickrapp/GetImageOnClickListener.java
+++ app/src/main/java/com/example/flickrapp/GetImageOnClickListener.java
@@ -11,25 +11,44 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Recover an image from Flickr API on click.
+ * Use AsyncTasks
+ */
 public class GetImageOnClickListener implements View.OnClickListener {
     static final String TAG = "GetImageOnClick";
     String urlAddress;
     ImageView iv;
 
+    /**
+     * Constructor
+     *
+     * @param iv the imageView to update
+     */
     public GetImageOnClickListener(ImageView iv) {
         this.urlAddress = "https://www.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json";
         this.iv = iv;
     }
 
+    /**
+     * Recover an image from Flickr API on click
+     *
+     * @param v the button
+     */
     @Override
     public void onClick(View v) {
+        // Check if the image view exist
         if (iv != null) {
             Bitmap bmp = null;
+
+            // Try to recover an image
             try {
                 bmp = getImage(getImageLink(0));
             } catch (ExecutionException | InterruptedException | TimeoutException | JSONException e) {
                 e.printStackTrace();
             }
+
+            // If it succeed, scale the image and send it to the imageVew
             if (bmp != null) {
                 Bitmap bmpDisplay = Bitmap.createScaledBitmap(bmp, 600, 600, true);
                 iv.setImageBitmap(bmpDisplay);
@@ -40,18 +59,32 @@ public class GetImageOnClickListener implements View.OnClickListener {
         }
     }
 
-    public JSONObject getImageList(String link) throws InterruptedException, ExecutionException, TimeoutException {
+    /**
+     * Ask the  Flickr API for a JSON describing a list of images.
+     * Use an AsyncTask (AsyncFlickrJSONData).
+     *
+     * @return Flickr answer as a JSONObject
+     */
+    public JSONObject getImageList() throws InterruptedException, ExecutionException, TimeoutException {
         AsyncFlickrJSONData task = (AsyncFlickrJSONData) new AsyncFlickrJSONData().execute(urlAddress);
         JSONObject images = task.get();
         return images;
     }
 
+    /**
+     * @param index the index of the image inside the JSON list
+     * @return the link to an image from the Flickr API
+     */
     public String getImageLink(int index) throws JSONException, InterruptedException, ExecutionException, TimeoutException {
-        String link = getImageList(urlAddress).getJSONArray("items").getJSONObject(index).getJSONObject("media").getString("m");
-        // String link = getImageList(urlAddress).getJSONArray("items").getJSONObject(index).getString("link");
+        String link = getImageList().getJSONArray("items").getJSONObject(index).getJSONObject("media").getString("m");
         return link;
     }
 
+    /**
+     * @param link the link of an image
+     * @returnthe image as a Bitmap
+     * use an AsyncTask (AsyncBitmapDownloader)
+     */
     public Bitmap getImage(String link) throws ExecutionException, InterruptedException {
         AsyncBitmapDownloader downloader = (AsyncBitmapDownloader) new AsyncBitmapDownloader().execute(link);
         Bitmap map = downloader.get();
--- app/src/main/java/com/example/flickrapp/GetLocationImageOnClickListener.java
+++ app/src/main/java/com/example/flickrapp/GetLocationImageOnClickListener.java
@@ -9,10 +9,22 @@ import org.json.JSONObject;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * Recover a geo localised image from Flickr API on click.
+ * Use AsyncTasks.
+ * Extends GetImageOnClickListener as the only differences are the url to send
+ * and the JSON answer structure.
+ */
 class GetLocationImageOnClickListener extends GetImageOnClickListener {
     static final String TAG = "GetLocationImageOnClick";
     MainActivity parent;
 
+    /**
+     * Constructor
+     *
+     * @param iv           the imageView to update
+     * @param mainActivity the parent activity. Used to recover the coordinates
+     */
     public GetLocationImageOnClickListener(ImageView iv, MainActivity mainActivity) {
         super(iv);
         parent = mainActivity;
@@ -22,19 +34,30 @@ class GetLocationImageOnClickListener extends GetImageOnClickListener {
                 + "&per_page=1&format=json";
     }
 
+    /**
+     * @param index the index of the image inside the JSON list
+     * @return the link to a geo localised image from the Flickr API
+     * The override is there to deal with a different JSON structure
+     */
+    @Override
     public String getImageLink(int index) throws JSONException, InterruptedException, ExecutionException, TimeoutException {
+        // Update the url with the last coordinates
         urlAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&license=4&api_key=4167810fbc47cc7f1da6d151edf224d5&has_geo=1&"
                 + "lat=" + parent.getLatitude()
                 + "&lon=" + parent.getLongitude()
                 + "&per_page=1&format=json";
-        // Photo link format : https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
-        JSONObject photo = getImageList(urlAddress).getJSONObject("photos").getJSONArray("photo").getJSONObject(index);
+
+        // Send the geo localised search request to Flickr and extract the info
+        JSONObject photo = getImageList().getJSONObject("photos").getJSONArray("photo").getJSONObject(index);
         String server_id = photo.getString("server");
         String id = photo.getString("id");
         String secret = photo.getString("secret");
 
+        // Use the recovered info to build the image url
+        // Photo link format : https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
         String link = "https://live.staticflickr.com/" + server_id + "/" + id + "_" + secret + ".jpg";
         Log.i("TAG", link);
+
         return link;
     }
 
