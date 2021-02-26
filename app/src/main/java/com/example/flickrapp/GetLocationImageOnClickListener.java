6202e964957e513e13e8f6c615e8e0b599870471
Nicolas Guilaume Soulié <nicolas.guillaume.soulie@efrei.net>
2021-02-26 15:42:16 +0100

    Get Image location now use geo location

--- app/src/main/java/com/example/flickrapp/GetLocationImageOnClickListener.java
+++ app/src/main/java/com/example/flickrapp/GetLocationImageOnClickListener.java
@@ -3,8 +3,6 @@ package com.example.flickrapp;
 import android.util.Log;
 import android.widget.ImageView;
 
-import androidx.loader.content.CursorLoader;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,13 +11,22 @@ import java.util.concurrent.TimeoutException;
 
 class GetLocationImageOnClickListener extends GetImageOnClickListener {
     static final String TAG = "GetLocationImageOnClick";
+    MainActivity parent;
 
-    public GetLocationImageOnClickListener(ImageView iv) {
+    public GetLocationImageOnClickListener(ImageView iv, MainActivity mainActivity) {
         super(iv);
-        urlAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&license=4&api_key=4167810fbc47cc7f1da6d151edf224d5&has_geo=1&lat=37.421998333333335&lon=-122.084&per_page=1&format=json";
+        parent = mainActivity;
+        urlAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&license=4&api_key=4167810fbc47cc7f1da6d151edf224d5&has_geo=1&"
+                + "lat=" + parent.getLatitude()
+                + "&lon=" + parent.getLongitude()
+                + "&per_page=1&format=json";
     }
 
     public String getImageLink(int index) throws JSONException, InterruptedException, ExecutionException, TimeoutException {
+        urlAddress = "https://api.flickr.com/services/rest/?method=flickr.photos.search&license=4&api_key=4167810fbc47cc7f1da6d151edf224d5&has_geo=1&"
+                + "lat=" + parent.getLatitude()
+                + "&lon=" + parent.getLongitude()
+                + "&per_page=1&format=json";
         // Photo link format : https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
         JSONObject photo = getImageList(urlAddress).getJSONObject("photos").getJSONArray("photo").getJSONObject(index);
         String server_id = photo.getString("server");
--- app/src/main/java/com/example/flickrapp/MainActivity.java
+++ app/src/main/java/com/example/flickrapp/MainActivity.java
@@ -29,33 +29,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            ActivityCompat.requestPermissions(this, new String[]{
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    1);
-            // here to request the missing permissions, and then overriding
-            // public void onRequestPermissionsResult ( int requestCode, String[] permissions,
-            // int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
         image = findViewById(R.id.image);
+        // Prepare location related actions & views
+        setUpLocationListener();
         getLocationText = findViewById(R.id.getLocationText);
+        getLocationImageButton = findViewById(R.id.getLocationImageButton);
+        getLocationImageButton.setOnClickListener(new GetLocationImageOnClickListener(image, this));
+        // Prepare the Get Image Button
         getImageButton = findViewById(R.id.getImageButton);
         getImageButton.setOnClickListener(new GetImageOnClickListener(image));
-
-        getLocationImageButton = findViewById(R.id.getLocationImageButton);
-        getLocationImageButton.setOnClickListener(new GetLocationImageOnClickListener(image));
-
+        // Prepare the Image List button
         listButton = findViewById(R.id.listButton);
         listButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +46,30 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 startActivity(new Intent(MainActivity.this, ListActivity.class));
             }
         });
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    private void setUpLocationListener() {
+        latitude = 48.8;
+        longitude = 2.4;
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
     @Override
